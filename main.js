75dab06dcc9b1106cb80408bcc81d21ad2467c3f
Badbatunde <hb.oyesile@gmail.com>
2022-08-15 21:33:29 +0100

    Added the draw function, and addEventListeners to the canvas in order to draw on the canvas

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext('2d');
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
 // Set the size of the canvas
 canvas.width = window.innerWidth;
@@ -8,7 +8,7 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = 'blue';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = '100';
+ctx.lineWidth = 100;
 
 let direction = true;
 let isDrawing = false;
@@ -18,8 +18,44 @@ let lastY = 0;
 
 function draw (e) {
     if (!isDrawing) return;
-    
+    ctx.strokeStyle =  `hsl(${hue}, 100%, 50%)`;
+
+    // Begins path
+    ctx.beginPath();
+
+    // Set the movement
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+
+    // Initializes the canvas
+    ctx.stroke();
+
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
 }
-ctx.moveTo = (lastX, lastY);
-ctx.lineTo = (e.offsetX, e.offsetY);
-ctx.stroke();
+
+canvas.addEventListener('mousedown', (e) => {
+isDrawing = true;
+lastX = e.offsetX;
+lastY = e.offsetY;
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => (isDrawing = false));
+canvas.addEventListener('mouseout', () => (isDrawing = false));
+
